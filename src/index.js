c69aadb93fcf9105b130894a3b215daaaa748c11
vmirella <vmirella_17@hotmail.com>
2018-10-01 18:47:47 -0500

    Estableciendo boilerplate

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"></link>
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
