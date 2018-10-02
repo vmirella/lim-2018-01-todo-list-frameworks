33bf05e7d5eda4f98e5f38b63c050da9cbc0b643
unknown <vmirella_17@hotmail.com>
2018-10-02 12:23:37 -0500

    Guardando datos en firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { InputGroup, InputGroupAddon, InputGroupText, Input, Button, Form, FormGroup, Row, FormText, Container, Col } from 'reactstrap';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDtrLufvwFbd_C69ALwHfYrrSKMKaDNHfo",
+  authDomain: "to-do-list-22a45.firebaseapp.com",
+  databaseURL: "https://to-do-list-22a45.firebaseio.com",
+  projectId: "to-do-list-22a45",
+  storageBucket: "to-do-list-22a45.appspot.com",
+  messagingSenderId: "478219269097"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      ingredient: '',
+      ingredients: []
+    } 
+  }
+
+  handleIngredientChange(e) {
+    this.setState({ ingredient: e.target.value });
+  }
+
+  addIngredient() {
+    let ingredient = this.state.ingredient;
+
+    let uid = firebase.database().ref().child('ingredients').push().key;
+
+    firebase.database().ref('ingredients').set({
+      uid: uid,
+      name: ingredient,
+      check : 0
+    });
+  }
+
+  componentDidMount() {
+    firebase.database().ref('ingredients').on('value', (snapshot) => {
+      let data = snapshot.val();
+      let ingredients = Object.values(data);
+      this.setState({ingredients});
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <Container className="App">
+        <header>
+          <h1>To-Do List</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>
+        Ingredientes para hacer un keke.
         </p>
-      </div>
+        <Row>
+          <Col sm={10}>
+            <Input className="input-ingredient" value={this.state.ingredient} onChange={this.handleIngredientChange.bind(this)}/>
+          </Col>
+          <Col sm={2}>
+            <Button type="button" color="default" onClick={this.addIngredient()}><i className="fas fa-plus"></i></Button>{' '}
+            <Button type="button" color="default"><i className="far fa-trash-alt"></i></Button>
+          </Col>
+        </Row>
+        <Row>
+          {this.state.ingredients.map((ingredient) => {
+            return (
+              <div>
+                {ingredient.name}
+              </div>
+            )
+          })}
+        </Row>
+      </Container>
     );
   }
 }
