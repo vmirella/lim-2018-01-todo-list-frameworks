ed2e31091470cc13a79a6252e0eaca40d57dcdfa
unknown <vmirella_17@hotmail.com>
2018-10-03 02:51:59 -0500

    implementando funcinalidades añadir y eliminar de firebase

--- src/App.js
+++ src/App.js
@@ -1,59 +1,65 @@
 import React, { Component } from 'react';
 import './App.css';
-import List from './List';
-import {Input, Button, Row, Container, Col, Form } from 'reactstrap';
+import {Input, Button, Row, Container, Col, Form, ListGroup, ListGroupItem } from 'reactstrap';
 import * as firebase from 'firebase';
-
-const config = {
-  apiKey: "AIzaSyDtrLufvwFbd_C69ALwHfYrrSKMKaDNHfo",
-  authDomain: "to-do-list-22a45.firebaseapp.com",
-  databaseURL: "https://to-do-list-22a45.firebaseio.com",
-  projectId: "to-do-list-22a45",
-  storageBucket: "to-do-list-22a45.appspot.com",
-  messagingSenderId: "478219269097"
-};
-firebase.initializeApp(config);
 
 class App extends Component {
 
   constructor() {
     super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.deleteAll = this.deleteAll.bind(this);
     this.state = {
       ingredient: '',
       ingredients: []
     } 
   }
 
-  /* handleSubmit(e) {
-    e.preventDefault();
-    this.setState({ ingredient: e.target.value });
-  } */
-
   handleIngredientChange(e) {
     this.setState({ ingredient: e.target.value });
   }
 
-  addIngredient() {
+  handleSubmit(e) {
+    e.preventDefault();
     let ingredient = this.state.ingredient;
-    let uid = firebase.database().ref().child('ingredients').push().key;
-    firebase.database().ref('ingredients').set({
-      uid: uid,
-      name: ingredient,
-      check : 0
+    if(ingredient !== ''){
+      let uid = firebase.database().ref().child('ingredients').push().key;
+      firebase.database().ref('ingredients/'+uid).set({
+        uid: uid,
+        name: ingredient,
+        check : 0
+      });
+      this.setState({ ingredient: ''});
+      this.inputIngredient.focus();
+    }
+  }
+
+  deleteAll() {
+    firebase.database().ref('ingredients').remove();
+    this.setState({ingredients: []});
+  }
+
+  deleteIngredient(uid) {
+    firebase.database().ref('ingredients/'+uid).remove();
+  }
+
+  checkIngredient(uid) {
+    firebase.database().ref('ingredients/'+uid).update({
+      check : 1
     });
   }
 
   componentDidMount() {
     firebase.database().ref('ingredients').on('value', (snapshot) => {
       let data = snapshot.val();
-      let ingredients = Object.values(data);
-      this.setState({ingredients});
+      if(data !== null){
+        let ingredients = Object.values(data);
+        this.setState({ingredients});
+      }
     });
   }
  
   render() {
-    console.log(this.state.ingredients)
-
     return (
       <Container className="App">
         <header>
@@ -62,18 +68,26 @@ class App extends Component {
         <p>
           Ingredientes para hacer un keke.
         </p>
-        <Row onSubmit="handleSubmit">
-          <Form>
-            <Col sm={10}>
-              <Input className="input-ingredient" value={this.state.ingredient}/>
+        <Form onSubmit={this.handleSubmit}>
+          <Row>
+            <Col sm={12}>
+              <Input autoFocus className="input-ingredient" value={this.state.ingredient} onChange={this.handleIngredientChange.bind(this)} ref={(Input) => { this.inputIngredient = Input }} />
+              <Button type="submit" color="default" className="add"><i className="fas fa-plus"></i></Button>
+              <Button type="button" color="default" className="delete" onClick={this.deleteAll}><i className="far fa-trash-alt"></i></Button>
             </Col>
-            <Col sm={2}>
-              <Button type="button" color="default" onClick={this.addIngredient()}><i className="fas fa-plus"></i></Button>{' '}
-              <Button type="button" color="default"><i className="far fa-trash-alt"></i></Button>
-            </Col>
-          </Form>
-        </Row>
-        <List ingredientes = {['harina','huevos']}/>
+          </Row>
+        </Form>  
+        <ListGroup className="list-group">
+          {this.state.ingredients.map((ingredient, i) => {
+            return (
+              <ListGroupItem key={i}>
+                <span>{ingredient.name}</span>
+                <Button type="button" color="default" className="check" onClick={() => this.checkIngredient(ingredient.uid)}><i className="fas fa-check"></i></Button>
+                <Button type="button" color="default" className="delete-item" onClick={() => this.deleteIngredient(ingredient.uid)}><i className="far fa-trash-alt"></i></Button>
+              </ListGroupItem>
+            )
+          })}          
+        </ListGroup>
       </Container>
     );
   }
